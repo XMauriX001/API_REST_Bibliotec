8228089209551a3c2d43433f52308465e49ef53b
XMauriX001 <mauriciorecinos021176@gmail.com>
2026-02-19 09:27:36 -0600

    cambio

--- Api_Rest/app/Http/Controllers/PrestamoController.php
+++ Api_Rest/app/Http/Controllers/PrestamoController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Http\Requests\BookRequest;
+
 
 class PrestamoController extends Controller
 {
-    //
+    /*
+    Función para hacer un prestamo de un libro
+    */
+    public function index(Request $request)
+    {
+        // Hacer el prestamo con el id del libro
+        $book = Book::find($request->input('book_id'));
+        $book->disponible = false;
+        $book->save();
+        return response()->json(['message' => 'Prestamo realizado correctamente'], 200);
+    }
 }
--- Api_Rest/routes/api.php
+++ Api_Rest/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\CatalogoController;
+use App\Http\Controllers\PrestamoController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::get('/books', [CatalogoController::class, 'index']);
+    Route::post('/loan', [PrestamoController::class, 'index']);
 });
